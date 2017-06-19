1443659c950c819c4ad0f0ec407e0b196a1c657f
Noah Reinagel <noah.r9@gmail.com>
2017-06-19 16:57:02 -0700

    Tried out aqws jdk it is too big to be viable

--- src/Handlers/GetHandler.java
+++ src/Handlers/GetHandler.java
@@ -1,13 +1,23 @@
 package Handlers;
 
+import InputStreamToString;
 import IncomingRequests.GetRequest;
 import OutgoingResponses.GetResponse;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 import rpc.RpcCallHandler;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class GetHandler extends RpcCallHandler<GetRequest, GetResponse> {
-    public GetHandler() {
+    public GetHandler(AmazonS3 s3) {
         super("/get", GetRequest.class, x -> {
-            return new GetResponse(x.RequestId, "");
+            S3Object obj = s3.getObject(new GetObjectRequest("custom-magic-sets", x.getId()));
+            InputStreamReader reader = new InputStreamReader(s3Ob)
+            InputStream stream = obj..getObjectContent();
+            return new GetResponse(x.RequestId, new InputStreamToString(obj.getObjectContent()));
         });
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,12 @@
 import Handlers.GetHandler;
 import Handlers.ListHandler;
 import Handlers.PutHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class main {
     public static void main(String[] args) {
-        new HttpJsonRpcApi(9039, new GetHandler(), new PutHandler(), new ListHandler()).start();
+        AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        new HttpJsonRpcApi(9039, new GetHandler(s3), new PutHandler(), new ListHandler()).start();
     }
 }
